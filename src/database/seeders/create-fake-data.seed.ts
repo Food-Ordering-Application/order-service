9667095d9f04cb413e2444f996ad5998dda774b9
ADMIN\Admin <thachdau16t@gmail.com>
2021-05-04 19:12:06 +0700

    update seed

--- src/database/seeders/create-fake-data.seed.ts
+++ src/database/seeders/create-fake-data.seed.ts
@@ -13,51 +13,55 @@ export default class CreateFakeData implements Seeder {
     const draftOrders = await factory(Order)({
       paymentType: PType.COD,
       orderStatus: OrdStatus.DRAFT,
-    }).createMany(30);
-    const orderedOrders = await factory(Order)({
-      paymentType: PType.COD,
-      orderStatus: OrdStatus.ORDERED,
-    }).createMany(30);
-    const completedOrders = await factory(Order)({
-      paymentType: PType.COD,
-      orderStatus: OrdStatus.COMPLETED,
-    }).createMany(60);
-    const cancelledOrders = await factory(Order)({
-      paymentType: PType.COD,
-      orderStatus: OrdStatus.CANCELLED,
-    }).createMany(30);
+    }).create();
+    // const draftOrders = await factory(Order)({
+    //   paymentType: PType.COD,
+    //   orderStatus: OrdStatus.DRAFT,
+    // }).createMany(30);
+    // const orderedOrders = await factory(Order)({
+    //   paymentType: PType.COD,
+    //   orderStatus: OrdStatus.ORDERED,
+    // }).createMany(30);
+    // const completedOrders = await factory(Order)({
+    //   paymentType: PType.COD,
+    //   orderStatus: OrdStatus.COMPLETED,
+    // }).createMany(60);
+    // const cancelledOrders = await factory(Order)({
+    //   paymentType: PType.COD,
+    //   orderStatus: OrdStatus.CANCELLED,
+    // }).createMany(30);
 
-    // ORDERED và 30 order COMPLETED tạo DELIVERY
-    const combineOrders = [...orderedOrders, ...completedOrders.slice(0, 30)];
+    // // ORDERED và 30 order COMPLETED tạo DELIVERY
+    // const combineOrders = [...orderedOrders, ...completedOrders.slice(0, 30)];
 
-    for (const order of combineOrders) {
-      await factory(Delivery)({
-        order: order,
-        deliveryStatus: _.sample(
-          Object.values(DeliveryStatus),
-        ) as DeliveryStatus,
-      }).create();
-    }
+    // for (const order of combineOrders) {
+    //   await factory(Delivery)({
+    //     order: order,
+    //     deliveryStatus: _.sample(
+    //       Object.values(DeliveryStatus),
+    //     ) as DeliveryStatus,
+    //   }).create();
+    // }
 
-    const orders = [
-      ...draftOrders,
-      ...orderedOrders,
-      ...completedOrders,
-      ...cancelledOrders,
-    ];
+    // const orders = [
+    //   ...draftOrders,
+    //   ...orderedOrders,
+    //   ...completedOrders,
+    //   ...cancelledOrders,
+    // ];
 
-    // Với mỗi Order tạo nhiều OrderItems
-    for (const order of orders) {
-      const orderItems = await factory(OrderItem)({
-        order: order,
-      }).createMany(5);
+    // // Với mỗi Order tạo nhiều OrderItems
+    // for (const order of orders) {
+    //   const orderItems = await factory(OrderItem)({
+    //     order: order,
+    //   }).createMany(5);
 
-      // Với mỗi OrderItem tạo nhiều OrderItemToppings
-      for (const orderItem of orderItems) {
-        await factory(OrderItemTopping)({
-          orderItem: orderItem,
-        }).createMany(Math.floor(Math.random() * 4));
-      }
-    }
+    //   // Với mỗi OrderItem tạo nhiều OrderItemToppings
+    //   for (const orderItem of orderItems) {
+    //     await factory(OrderItemTopping)({
+    //       orderItem: orderItem,
+    //     }).createMany(Math.floor(Math.random() * 4));
+    //   }
+    // }
   }
 }
