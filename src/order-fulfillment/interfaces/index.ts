4378a57dd8087e8bfde9a3f90e49b58cbbda9bd1
mrjared16 <doannguyentanphuc99@gmail.com>
2021-05-29 18:04:22 +0700

    add void order dto and interfaces

--- src/order-fulfillment/interfaces/index.ts
+++ src/order-fulfillment/interfaces/index.ts
@@ -1,3 +1,4 @@
 export { IRestaurantConfirmOrderResponse } from './restaurant-confirm-order-response.interface';
+export { IRestaurantVoidOrderResponse } from './restaurant-void-order-response.interface';
 export { IDriverCompleteOrderResponse } from './driver-complete-order-response.interface';
 export { IDriverPickedUpOrderResponse } from './driver-picked-up-order-response.interface';
---
+++ src/order-fulfillment/interfaces/restaurant-void-order-response.interface.ts
@@ -0,0 +1,4 @@
+export interface IRestaurantVoidOrderResponse {
+  status: number;
+  message: string;
+}
