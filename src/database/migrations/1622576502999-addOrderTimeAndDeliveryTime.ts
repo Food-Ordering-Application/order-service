2f2368c2f4c4a355aef81bf7a4a127bdecf01c16
mrjared16 <doannguyentanphuc99@gmail.com>
2021-06-03 02:17:22 +0700

    init orderTime column in migration

--- src/database/migrations/1622576502999-addOrderTimeAndDeliveryTime.ts
+++ src/database/migrations/1622576502999-addOrderTimeAndDeliveryTime.ts
@@ -1,16 +1,24 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class addOrderTimeAndDeliveryTime1622576502999 implements MigrationInterface {
-    name = 'addOrderTimeAndDeliveryTime1622576502999'
+export class addOrderTimeAndDeliveryTime1622576502999
+  implements MigrationInterface
+{
+  name = 'addOrderTimeAndDeliveryTime1622576502999';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "delivery" ADD "orderTime" TIMESTAMP`);
-        await queryRunner.query(`ALTER TABLE "delivery" ADD "expectedDeliveryTime" TIMESTAMP`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "delivery" ADD "orderTime" TIMESTAMP`);
+    await queryRunner.query(
+      `ALTER TABLE "delivery" ADD "expectedDeliveryTime" TIMESTAMP`,
+    );
+    await queryRunner.query(
+      `update delivery set "orderTime" = delivery."updatedAt", "expectedDeliveryTime" = "updatedAt"`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "delivery" DROP COLUMN "expectedDeliveryTime"`);
-        await queryRunner.query(`ALTER TABLE "delivery" DROP COLUMN "orderTime"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "delivery" DROP COLUMN "expectedDeliveryTime"`,
+    );
+    await queryRunner.query(`ALTER TABLE "delivery" DROP COLUMN "orderTime"`);
+  }
 }
