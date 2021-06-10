317239459595160c7a9e90a47501ed0bd7b6ab49
hienhhcc <thachdau16t@gmail.com>
2021-06-10 18:05:49 +0700

    Update order.service.ts

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -62,6 +62,7 @@ import * as paypal from '@paypal/checkout-server-sdk';
 import { client } from '../config/paypal';
 import axios from 'axios';
 import * as uniqid from 'uniqid';
+import momenttimezone from 'moment-timezone';
 import {
   DELIVERY_SERVICE,
   NOTIFICATION_SERVICE,
@@ -71,35 +72,6 @@ import { ClientProxy } from '@nestjs/microservices';
 import { filteredOrder, allowed } from '../shared/filteredOrder';
 const DEFAULT_EXCHANGE_RATE = 0.00004;
 const PERCENT_PLATFORM_FEE = 0.2;
-
-// import * as paypalRest from 'paypal-rest-sdk';
-
-// paypalRest.configure({
-//   mode: 'sandbox', // Sandbox or live
-//   client_id: process.env.PAYPAL_CLIENT_ID,
-//   client_secret: process.env.PAYPAL_CLIENT_SECRET,
-// });
-
-// console.log('NODE_ENV', process.env.NODE_ENV);
-
-// const webhook_json = {
-//   url: 'https://apigway.herokuapp.com/order/events',
-//   event_types: [
-//     {
-//       name: 'CHECKOUT.ORDER.APPROVED',
-//     },
-//   ],
-// };
-
-// paypalRest.notification.webhook.create(webhook_json, function (error, webhook) {
-//   if (error) {
-//     console.error(JSON.stringify(error.response));
-//     // throw error;
-//   } else {
-//     console.log('Create webhook Response');
-//     console.log(webhook);
-//   }
-// });
 
 @Injectable()
 export class OrderService {
@@ -1289,20 +1261,21 @@ export class OrderService {
         });
 
       if (from && to) {
-        const fromDate = new Date(from);
-        const toDate = new Date(to);
+        const fromDate = momenttimezone
+          .tz(from, 'Asia/Ho_Chi_Minh')
+          .utc()
+          .format();
+        const toDate = momenttimezone.tz(to, 'Asia/Ho_Chi_Minh').utc().format();
 
-        console.log('FromDate', fromDate);
-        console.log('ToDate', toDate);
-        console.log('FromDateISO', fromDate.toISOString());
-        console.log('ToDateISO', toDate.toISOString());
+        console.log('fromdate', fromDate);
+        console.log('todate', toDate);
 
         orderQueryBuilder = orderQueryBuilder
           .andWhere('order.createdAt >= :startDate', {
-            startDate: fromDate.toISOString(),
+            startDate: fromDate,
           })
           .andWhere('order.createdAt <= :endDate', {
-            endDate: toDate.toISOString(),
+            endDate: toDate,
           });
       }
 
@@ -1396,15 +1369,18 @@ export class OrderService {
     }
 
     if (from && to) {
-      const fromDate = new Date(from);
-      const toDate = new Date(to);
+      const fromDate = momenttimezone
+        .tz(from, 'Asia/Ho_Chi_Minh')
+        .utc()
+        .format();
+      const toDate = momenttimezone.tz(to, 'Asia/Ho_Chi_Minh').utc().format();
 
       orderQueryBuilder = orderQueryBuilder
         .andWhere('delivery.orderTime >= :startDate', {
-          startDate: fromDate.toISOString(),
+          startDate: fromDate,
         })
         .andWhere('delivery.orderTime <= :endDate', {
-          endDate: toDate.toISOString(),
+          endDate: toDate,
         });
     }
 
